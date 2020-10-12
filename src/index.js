e5afdd959ec3eb073f7a3802fcc5bb88caf2cc72
Scott Hill <scott289@gmail.com>
2020-10-11 17:29:53 -0700

    started

--- src/index.js
+++ src/index.js
@@ -1,14 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import {
+  useQuery,
+  useMutation,
+  QueryCache,
+  ReactQueryCacheProvider,
+} from "react-query";
+import { ReactQueryDevtools } from "react-query-devtools";
+
+const queryCache = new QueryCache();
+
+const MovieList = () => {
+  const { isLoading, error, data } = useQuery("movieData", () =>
+    fetch("/api/movies").then((res) => res.json())
+  );
+
+  if (isLoading) return "Loading...";
+  if (error) return "An error has occurred: " + error.message;
+
+  return (
+    <div>
+      {data &&
+        data.map((movie) => (
+          <div key={movie.id}>
+            <div>{movie.title}</div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+const MovieAdd = () => {
+  const [title, setTitle] = React.useState("");
+
+  // const [mutatePostTodo] = useMutation(
+  //   (text) => axios.post("/api/data", { text }),
+  //   {
+  //     onSuccess: () => {
+  //       // Query Invalidations
+  //       // queryCache.invalidateQueries('todos')
+  //       setText("");
+  //     },
+  //   }
+  // );
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("SUBMIT!");
+          // mutatePostTodo(text);
+        }}
+      >
+        title:{" "}
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
 
 const Index = () => {
   return (
-    <div>
-      Hello React! You are ready for heroku
-      <section>
-        <a href="/api/movies">/api/movies</a>
-      </section>
-    </div>
+    <>
+      <ReactQueryCacheProvider queryCache={queryCache}>
+        Hello React! You are ready for heroku
+        <section>
+          <a href="/api/movies">/api/movies</a>
+        </section>
+        <MovieList />
+        <MovieAdd />
+      </ReactQueryCacheProvider>
+      <ReactQueryDevtools initialIsOpen />
+    </>
   );
 };
 
