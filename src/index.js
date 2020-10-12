6dab26afad42c234a92d2509c48b85981ed17ba5
Scott Hill <scott289@gmail.com>
2020-10-11 21:04:11 -0700

    hooks and getAll

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import MovieList from "./MovieList";
+import { getMovieList } from "./MovieApi";
 
 const Index = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((data) => setMovies(data));
+  }, []);
+
   return (
     <div>
-      Hello React! You are ready for heroku
+      <h1>Movies</h1>
       <section>
         <a href="/api/movies">/api/movies</a>
       </section>
+      <MovieList movies={movies} />
     </div>
   );
 };
